7f15d8cfc202937725e351285e15701308e88ae7
jbrusq <jbrusq@redhat.com>
2026-04-14 04:38:15 -0400

    clean

--- lab_eda/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-ansible-builder build -f lab_eda.yml -t aap-pah.my-lab.local/lab_eda
-
--- lab_ee/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-ansible-builder build -f lab_ee.yml -t aap-pah.my-lab.local/lab_ee -v 2
-
